11d42a9863eba4d4c7d792df895ecc153a7960e2
Gustavo Coelho <gutorc@hotmail.com>
2015-05-12 22:28:35 -0300

    The send.pm tried to use authetication.
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- send.pm
+++ send.pm
@@ -2,7 +2,8 @@
 
 use strict;
 use warnings;
-
+use Data::Dumper;
+use WWW::Mechanize;
 use LWP;
 open( my $fh,"<", "key") or die("Meu cu");
 my $key;
@@ -39,3 +40,25 @@ my $res = $browser->request($req);
 
 print $res->as_string;
 print $res->content;
+
+my $reque = HTTP::Request->new(GET=>"https://www.urionlinejudge.com.br/judge/");
+my $res2 = $browser->request($reque);
+
+ 
+print $res2->as_string;
+print $res2->content;
+
+my $mech = WWW::Mechanize->new();
+$mech->agent_alias("Linux Mozilla");
+$mech->get("https://www.urionlinejudge.com.br/judge/login");
+my $form = $mech->form_id("UserLoginForm");
+$form->dump;
+$mech->field("data[User][email]",$username);
+$mech->field("data[User][password]",$password);
+my $res3 = $mech->submit_form();
+if($res3->is_success){
+	$res3->decoded_content;
+}else{
+	$res3->status_line;
+}
+#print $res3->as_string;
