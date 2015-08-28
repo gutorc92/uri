b526fb5119ccb186c9e985cf0b5a00e766655075
Gustavo Coelho <gutorc@hotmail.com>
2015-08-27 23:10:10 -0300

    New jar file
    It uses the configurationfile with the password and
    email to submit a file.
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- submit.sh
+++ submit.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-
+jarFile="$HOME/Documents/uri/"
+oldPWD=$PWD
 problem=$(basename $PWD)
+
 file="$PWD/$problem.cpp"
 echo "You will submit the problem $problem"
 echo "The file $file will be used"
 
+cd "$jarFile"
+echo " Actual $PWD"
+
 if [ -e "$file" ];
 then
-   java -jar ../urisubmit-1.0-SNAPSHOT.jar "$file" "$problem"
+   java -jar "$jarFile/urisubmit-1.0-SNAPSHOT.jar" "$file" "$problem"
 else
     echo "There is no a cpp file"
 fi
  
-	
+cd "$oldPWD"
+echo "New $PWD"
 
