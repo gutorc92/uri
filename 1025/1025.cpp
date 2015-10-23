f6cc14fa033c0362d15476f5268370ebb46f45f3
Gustavo Coelho <gutorc@hotmail.com>
2015-10-22 22:12:54 -0200

    Using binary search
    In this trying: first, I sorted the array
    then I made a binary search on it. Howeaver,
    it does not work because I cannot know the size
    of the array after sorting. The size can deacrease
    due to the repeted numbers.
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- 1025/1025.cpp
+++ 1025/1025.cpp
@@ -5,13 +5,33 @@
 
 using namespace std;
 
+int buscabinaria(int x, int n, int* p){
+    int e, m, d;
+    e = 0;
+    d = n -1;
+    while(e <= d){
+        m = (e + d)/2;
+        if(p[m] == x){
+            return m;
+        }
+        if(p[m] < x){
+            e = m + 1;
+        }else{
+            d = m - 1;
+        }
+    }
+
+    return -1;
+
+}
+
+
 int main(int argc, char * argv[]){
     int n,q;
     int marmores[100005];
     int caso = 1;
     while(true){
 
-        fill(marmores,marmores+100005,-1);
         cin >> n >> q;
         if(n == 0 && q == 0){
             break;
@@ -19,20 +39,19 @@ int main(int argc, char * argv[]){
         int nu;
         for(int i = 1; i <= n ; i++){
             cin >> nu;
-            if(marmores[nu] == -1){
-                marmores[nu] = i; 
-            }
+            marmores[i] = nu; 
         };
+        sort(marmores,marmores+n);
         int con;
         cout << "CASE# " << caso << ":" << endl;
         for(int i = 0; i < q ; i++){
             cin >> con;
-            if(marmores[con] != -1){
-                cout << con << " found at " << marmores[con] << endl;
-            }else{
+            int pos = buscabinaria(con, n, marmores);
+            if(pos == -1){
                 cout << con << " not found" << endl;
-            } 
-             
+            } else{
+                cout << con <" found at " << pos + 1 << endl;
+            }
         };
         caso++; 
         
