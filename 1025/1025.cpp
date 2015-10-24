8e065331ecea53357b78bc18d6c34ee92ebf3fd1
Gustavo Coelho <gutorc@hotmail.com>
2015-10-24 11:53:42 -0200

    Codo to solve problem 1025 of URI
    The problem can be found at https://www.urionlinejudge.com.br/judge/pt/problems/view/1025.
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- 1025/1025.cpp
+++ 1025/1025.cpp
@@ -5,26 +5,22 @@
 
 using namespace std;
 
-int buscabinaria(int x, int n, int* p){
-    int e, m, d;
-    e = 0;
-    d = n -1;
-    while(e <= d){
-        m = (e + d)/2;
-        if(p[m] == x){
-            return m;
-        }
-        if(p[m] < x){
-            e = m + 1;
-        }else{
-            d = m - 1;
+int f(int* p,int n, int val){
+    int i = 1;
+    for(i = 1; i <= n; i++){
+        if(p[i] == val){
+            return i;
         }
     }
-
     return -1;
-
 }
 
+void print(int* p, int n){
+    for(int i = 1; i <= n; i++){
+        cout << p[i] << " ";
+    }
+    cout << endl;
+}
 
 int main(int argc, char * argv[]){
     int n,q;
@@ -41,16 +37,17 @@ int main(int argc, char * argv[]){
             cin >> nu;
             marmores[i] = nu; 
         };
-        sort(marmores,marmores+n);
+        sort(marmores,marmores+n+1);
+        //print(marmores, n);
         int con;
         cout << "CASE# " << caso << ":" << endl;
         for(int i = 0; i < q ; i++){
             cin >> con;
-            int pos = buscabinaria(con, n, marmores);
+            int pos = f(marmores,n,con);
             if(pos == -1){
-                cout << con << " not found" << endl;
-            } else{
-                cout << con <" found at " << pos + 1 << endl;
+                cout << con <<  " not found" << endl;
+            }else{
+                cout << con << " found at " << pos << endl;
             }
         };
         caso++; 
