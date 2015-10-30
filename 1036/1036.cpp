ea54399f20790aa0dcde4b3f8cb498c725a99af2
Gustavo Coelho <gutorc@hotmail.com>
2015-10-29 22:27:14 -0200

    Clean git
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ 1036/1036.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cmath>
+#include<stdio.h>
+
+using namespace std;
+
+int main(int argc, char * argv[]){
+	double a,b,c, r;
+	double t;
+	cin >> a >> b >> c;
+	cout.precision(5);
+
+	if( a == 0){
+		cout << "Impossivel calcular";
+		return 1;
+	}
+
+
+	r = pow(b,2) - (4 * a * c);
+
+	if( r < 0){
+		cout << "Impossivel calcular";
+		return 1;
+	}
+
+	if(r == 0){
+		t = (-b)/(2*a);
+		printf("R1 = %.5f\n",t);
+		printf("R2 = %.5f",t);
+		return 1;
+	}	
+	t = ((-b) + sqrt(r))/(2*a);
+	printf("R1 = %.5f\n",t);
+	t = ((-b) - sqrt(r))/(2*a);
+	printf("R2 = %.5f",t);
+
+	
+
+
+}
