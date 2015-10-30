61e101a3bbf5ad27b5b57ff7b4a8e85a34a55d98
Gustavo Coelho <gutorc@hotmail.com>
2015-10-29 22:58:23 -0200

    Code to solve the problem 1871 of URI
    It can be found at https://www.urionlinejudge.com.br/judge/pt/problems/view/1871
    
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- 1871/1871.cpp
+++ 1871/1871.cpp
@@ -5,14 +5,23 @@
 #include<algorithm>
 using namespace std;
 
+
+
 int main(int argc, char * argv[]){
     unsigned int m, n;
+    string s;
     cin >> m >> n;
     while(m != 0 && n != 0){
         unsigned int sum = m + n;
-        string s = to_string(sum);
-        replace(s.begin(),s.end(),'0','\0');
-        cout << s << endl;
+        s = to_string(sum);
+//cout << s << endl;
+        for(auto it = s.begin(); it != s.end(); it++){
+//cout << *it << endl;
+            if(*it != '0'){
+                cout << *it;
+            }
+        } 
+        cout << endl;
         cin >> m >> n;
 
     }	
