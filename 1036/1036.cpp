41556d29d0c908b27b6e21eaa3ae62180af1ff1a
Gustavo Coelho <gutorc@hotmail.com>
2017-08-15 21:14:05 -0300

    Problem 1036
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- 1036/1036.cpp
+++ 1036/1036.cpp
@@ -11,7 +11,7 @@ int main(int argc, char * argv[]){
 	cout.precision(5);
 
 	if( a == 0){
-		cout << "Impossivel calcular";
+		cout << "Impossivel calcular\n";
 		return 1;
 	}
 
@@ -19,20 +19,20 @@ int main(int argc, char * argv[]){
 	r = pow(b,2) - (4 * a * c);
 
 	if( r < 0){
-		cout << "Impossivel calcular";
+		cout << "Impossivel calcular\n";
 		return 1;
 	}
 
 	if(r == 0){
 		t = (-b)/(2*a);
 		printf("R1 = %.5f\n",t);
-		printf("R2 = %.5f",t);
+		printf("R2 = %.5f\n",t);
 		return 1;
 	}	
 	t = ((-b) + sqrt(r))/(2*a);
 	printf("R1 = %.5f\n",t);
 	t = ((-b) - sqrt(r))/(2*a);
-	printf("R2 = %.5f",t);
+	printf("R2 = %.5f\n",t);
 
 	
 
