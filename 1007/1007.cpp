e4169c6e69422f710a6745ce46bda0b6e01aa7af
Gustavo Coelho <gutorc@hotmail.com>
2017-08-20 01:51:35 -0300

    1007 problem
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ 1007/1007.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+
+    int A, B, C, D, diferenca;
+    cin >> A >> B >> C >> D;
+    diferenca = (A * B - C * D);
+    cout << "DIFERENCA = " << diferenca <<endl;
+    
+    return 1 ;
+}
